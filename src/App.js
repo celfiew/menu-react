94c4cc2acfa7f330331a75ca4fbe060c5af1b4a7
celfiew <celfiew@gmail.com>
2022-01-28 21:45:23 -0500

    veganiemonos

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
-const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+const allCategories = ['todos', ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
-    if (category === 'all') {
+    if (category === 'todos') {
       setMenuItems(items);
       return;
     }
@@ -21,8 +21,11 @@ function App() {
   return (
     <main>
       <section className='menu section'>
+        <section className='title-logo'>
+          <img src='./images/logoGreen 1.png' alt='logo' />
+        </section>
         <div className='title'>
-          <h2>our menu</h2>
+          <h2>Nuestro menú</h2>
           <div className='underline'></div>
         </div>
         <Categories categories={categories} filterItems={filterItems} />
